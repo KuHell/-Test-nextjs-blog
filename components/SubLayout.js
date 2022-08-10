a2b862c0340e9551b097237e0d3d41c96acdcac9
KuHell <itkdgus489@gmail.com>
2022-08-11 00:49:09 +0900

    set: scr, ssg Layout 및 subLayout으로 감싸서 실행

---
+++ components/SubLayout.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function SubLayout({ children }) {
+  return (
+    <>
+      <h1>
+        <Link href="/">
+          <a>Home 로</a>
+        </Link>
+        {children}
+      </h1>
+    </>
+  );
+}
