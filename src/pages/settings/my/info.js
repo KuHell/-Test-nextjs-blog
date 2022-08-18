9dc9ed91976c9f68e63a9d2c4e6649eae2a52afb
KuHell <itkdgus489@gmail.com>
2022-08-18 18:30:37 +0900

    api setting

--- src/pages/settings/my/info.js
+++ src/pages/settings/my/info.js
@@ -1,11 +1,17 @@
 import styles from '../../../../styles/Home.module.css'
 import Layout from 'components/Layout'
 import SubLayout from 'components/SubLayout'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 
 export default function Myinfo() {
+  const [clicked, setClicked] = useState(false)
+  const { status = 'initail' } = router.query
+
   return (
     <>
       <h1 className={(styles.title, styles.time)}>Myinfo</h1>
+      <h1 className={(styles.title, styles.time)}>clicked {clicked}</h1>
     </>
   )
 }
