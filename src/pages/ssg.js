aa3dc8210c016d2632dd288855ea36c04ab4919b
KuHell <itkdgus489@gmail.com>
2023-01-21 20:34:11 +0900

    reset

--- src/pages/ssg.js
+++ src/pages/ssg.js
@@ -8,7 +8,6 @@ import SubLayout from '../components/SubLayout'
 
 export async function getStaticProps() {
   console.log('server')
-  console.log('server')
   return {
     props: { time: new Date().toISOString() },
   }
