d5dd650fc0f6f02c6fb5acab30b8d3430bfc5d36
KuHell <itkdgus489@gmail.com>
2023-01-23 23:06:54 +0900

    client

--- src/pages/isr.js
+++ src/pages/isr.js
@@ -7,7 +7,7 @@ import Layout from '../components/Layout'
 import SubLayout from '../components/SubLayout'
 
 export async function getStaticProps() {
-  console.log('server')
+  console.log('client')
   return {
     props: { time: new Date().toISOString() },
     revalidate: 1,
